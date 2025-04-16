694720388327e9ebd3d9e23a7eb89b420b4787ca
aharley <harley.adam@gmail.com>
2025-04-16 01:04:31 -0700

    renamed var

--- download_reference_model.sh
+++ download_reference_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TARFILE="alltracker_reference.tar.gz"
-echo "downloading ${TARFILE} from dropbox"
-wget --max-redirect=20 -O ${TARFILE} https://www.dropbox.com/scl/fi/ng66ceortfy07bgie3r54/alltracker_reference.tar.gz?rlkey=o781im2v0sl7035hy8fcuv1d5&st=u5mcttcx&dl=1
+FILE="alltracker_reference.tar.gz"
+echo "downloading ${FILE} from dropbox"
+wget --max-redirect=20 -O ${FILE} https://www.dropbox.com/scl/fi/ng66ceortfy07bgie3r54/alltracker_reference.tar.gz?rlkey=o781im2v0sl7035hy8fcuv1d5&st=u5mcttcx&dl=1
